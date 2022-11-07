f842184595b6b76ffb946e7ece7b4189215f1847
whitish1984 <alzheimers.1864@gmail.com>
2022-11-07 23:27:45 +0000

    Fix test after refactoring.

--- tests/lib/main.test.ts
+++ tests/lib/main.test.ts
@@ -4,8 +4,6 @@ import type { Args } from '@/lib/main';
 import Handlebars from 'handlebars';
 import p from 'path';
 
-import { initInputPaths } from '@/lib/dataFetcher';
-import { initPreloads } from '@/lib/preload';
 import { tn, mapperFactory, requireFromString } from '#/testUtil'; 
 
 // test targets
@@ -17,22 +15,19 @@ describe('run', () => {
 
   // TEST DATA
   const minimum: Args = {
-    templates: ['path/to/tmpl.hbs.txt'],
-    outputDir: './out',
     templateDir: 'path/',
-    inputs: ['path/to/data.yaml'],
-    customHelpers: ['path/to/helper.js'],
-    preloads: ['path/to/preload.hbs.txt']
+    outputDir: './out'
   };
   const full: Args = {
-    templates: ['path/to/tmpl.hbs.txt'],
-    outputDir: './out',
     templateDir: 'path/',
+    outputDir: './out',
+    templates: ['path/to/tmpl.hbs.txt'],
+    blueprints: ['path/to/.blueprint'],
     inputs: ['path/to/data.yaml'],
     customHelpers: ['path/to/helper.js'],
     preloads: ['path/to/preload.hbs.txt']
   };
-  const STORE = { writeFile: '', mkDir: '' };
+  const STORE: Record<string, string> = {};
 
   beforeEach(() => {
     jest.mock(
@@ -43,9 +38,12 @@ describe('run', () => {
     );
     (jest.spyOn(fs, 'readFile') as jest.Mock<(path:string)=>Promise<string>>)
       .mockImplementation(mapperFactory<string>({
+        'path/to/.blueprint': '{{preload \'bp-preload.hbs.txt\'}}{{render \'bp-tmpl.txt\' \'bp-tmpl.hbs.txt\'}}',
         'path/to/data.yaml': 'hello: world\narr:\n  - 1\n  - 2\n  - 3\n',
-        'path/to/tmpl.hbs.txt': '{{first arr}}',
-        'path/to/preload.hbs.txt': '{{hello}}'
+        'path/to/tmpl.hbs.txt': '{{hello}}',
+        'path/to/bp-tmpl.hbs.txt': '{{hello}}',
+        'path/to/preload.hbs.txt': '{{first arr}}',
+        'path/to/bp-preload.hbs.txt': '{{#each arr as |el|}}{{el}}{{/each}}'
       }));
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (jest.spyOn(fs, 'stat') as jest.Mock<(path: string) => Promise<any>>)
@@ -53,28 +51,22 @@ describe('run', () => {
         'path/to/tmpl.hbs.txt': { isFile: () => true }
       }));
     (jest.spyOn(fs, 'writeFile') as jest.Mock<(path: string, out: string) => Promise<void>>)
-      .mockImplementation((_path, out) => { STORE.writeFile = out; return Promise.resolve(); });
-    (jest.spyOn(fs, 'mkdir') as jest.Mock<(path: string) => Promise<string>>)
-      .mockImplementation((path) => Promise.resolve(STORE.mkDir = path));
-    initInputPaths();
-    initPreloads();
+      .mockImplementation((path, out) => { STORE[path] = out; return Promise.resolve(); });
+    (jest.spyOn(fs, 'mkdir') as jest.Mock<(path: string, out: string) => Promise<void>>);
   });
 
   it(tn('runs well', 'if minimum properties are set for Args.'), async () => {
-  
     const result = await run(minimum);
     expect(result.consolidatedData)
-      .toEqual({_env: expect.any(Object), hello: 'world', arr: [1,2,3]});
+      .toEqual({_env: expect.any(Object)});
 
-    expect(result.registerdHelpers).toEqual(['first']);
-    expect(Handlebars.helpers).toMatchObject({ first: expect.any(Function) });
+    expect(result.registerdHelpers).toEqual([]);
 
-    expect(result.generatedFiles).toEqual(['out/to/tmpl.txt']);
-    expect(STORE).toEqual({writeFile: 'world1', mkDir: 'out/to'});
+    expect(result.generatedFiles).toEqual([]);
+    expect(STORE).toEqual({});
   });
 
   it(tn('runs well', 'if full properties are set for Args.'), async () => {
-  
     const result = await run(full);
     expect(result.consolidatedData)
       .toEqual({_env: expect.any(Object), hello: 'world', arr: [1,2,3]});
@@ -82,8 +74,8 @@ describe('run', () => {
     expect(result.registerdHelpers).toEqual(['first']);
     expect(Handlebars.helpers).toMatchObject({ first: expect.any(Function) });
 
-    expect(result.generatedFiles).toEqual(['out/to/tmpl.txt']);
-    expect(STORE).toEqual({writeFile: 'world1', mkDir: 'out/to'});
+    expect(result.generatedFiles).toEqual(['out/to/tmpl.txt', 'out/to/bp-tmpl.txt' ]);
+    expect(STORE).toEqual({'out/to/tmpl.txt': '1123world', 'out/to/bp-tmpl.txt': '1123world'});
   });
 
 });
